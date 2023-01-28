212fd47c4c17e7bac1235c2ca7f9faec0ba97b8d
dolevdr <dolevdr95@gmail.com>
2023-01-28 11:29:33 +0200

    Set headline and background color

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const pageStyle={
+    background: 'linear-gradient(to left, #04052E,#140152, #22007C)',
+    width:'100%',
+    height:window.innerHeight
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='page' style={pageStyle}>
+      <p>מה לובשים עכשיו</p>      
     </div>
   );
 }
